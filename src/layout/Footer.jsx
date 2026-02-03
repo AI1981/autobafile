71f83f6091a9863cb807ef50dbb38ba44eebcdb1
AI1981 <a.impastato@wiinet.it>
2026-02-03 19:05:21 +0100

    aggiuta powered by wiinet

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,8 +4,9 @@ import Button from "../components/Button";
 export const Footer = () => {
   return (
     <section className="relative overflow-hidden bg-slate-700 py-10">
-      <div className="container mx-auto px-6 text-center">
-        <span className="text-sm text-white">© 2025 Autobafile. Tutti i diritti riservati.</span>
+      <div className="container mx-auto px-6 text-center flex flex-col">
+        <span className="text-sm text-white">© 2026 Autobafile. Tutti i diritti riservati.</span>
+        <span className="text-sm text-white">Powerd by Wiinet</span>
       </div>
     </section>
   );
